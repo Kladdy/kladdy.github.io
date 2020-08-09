696059979cd979d44f91fe5d453ec5f47eb29b23
Sigfrid Stjärnholm <sigge.stjarnholm@gmail.com>
2020-08-10 01:45:02 +0200

    Updates

---
+++ precache-manifest.00e81c55d17edd3bb750f5f9d2b7edb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05e5f5fe6347da6d01e51a4b56ba048b",
+    "url": "/kladdy.github.io/index.html"
+  },
+  {
+    "revision": "4092b35523e0a234aef6",
+    "url": "/kladdy.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bf79e0fc48d59fd198f7",
+    "url": "/kladdy.github.io/static/js/2.4c736102.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kladdy.github.io/static/js/2.4c736102.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4092b35523e0a234aef6",
+    "url": "/kladdy.github.io/static/js/main.a8198eeb.chunk.js"
+  },
+  {
+    "revision": "c34e1b70d7bf93abf4f8",
+    "url": "/kladdy.github.io/static/js/runtime-main.e3431a56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kladdy.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
